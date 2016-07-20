5868c8fe321cd7147aa45ba760b03430ec056b15
GamaSquad <maila_mi@abv.bg>
2016-07-20 20:43:42 +0300

    JS  HW

---
+++ task-04.js
@@ -0,0 +1,4 @@
+function task4(){
+    var anchors = document.querySelectorAll('a');
+    alert(anchors[0].innerHTML);
+}
---
+++ task-06.js
@@ -0,0 +1,3 @@
+function task6(){
+    document.write("<img src=\"images\\img"+Math.floor(Math.random()*6)+".png\" />");
+}
